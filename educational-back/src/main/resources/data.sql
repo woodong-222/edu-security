4d10df00c8c457fa086ac5efd2dfccf8691e0ccd
강민구 <sorrysam7879@gmail.com>
2025-05-02 09:22:31 +0900

    insert server

---
+++ educational-back/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+/*DELETE FROM users;*/
+
+INSERT INTO users (username, password) VALUES ('admin', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
+INSERT INTO users (username, password) VALUES ('test', '88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589');
